9e10f57a9b60243e4e2919928a5e2abd07ff6390
‘Juliano <julianoperins@gmail.com>
2021-01-13 22:43:00 -0500

    Finished the first part of the navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Navbar.css";
 
+import { FaYoutube } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 const Navbar: React.FC = () => {
-  return <div className="navbarContainer"></div>;
+  return (
+    <div className="navbarContainer">
+      <div className="left">
+        <button className="btn-burger">
+          <GiHamburgerMenu id="burgerIcon" />
+        </button>
+        <FaYoutube id="youtubeIcon" />
+        <h1>Hellotube</h1>
+      </div>
+      <div className="center"></div>
+      <div className="right"></div>
+    </div>
+  );
 };
 
 export default Navbar;
