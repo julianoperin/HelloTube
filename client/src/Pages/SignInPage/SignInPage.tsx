67a5ae170e5b522f6d0f36a2981232bbf20a60e6
‘Juliano <julianoperins@gmail.com>
2021-01-15 17:52:51 -0500

    Successfully logged in with google OAuth

--- client/src/Pages/SignInPage/SignInPage.tsx
+++ client/src/Pages/SignInPage/SignInPage.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./SignInPage.css";
+import axios from "axios";
 
 import { GoogleLogin } from "react-google-login";
 
 const SignInPage: React.FC = () => {
   const responseGoogle = (res: any) => {
     console.log(res);
+
+    const name = res.profileObj.name;
+    const email = res.profileObj.email;
+    const googleId = res.profileObj.googleId;
+
+    axios
+      .post("http://localhost:3001/signin", {
+        name: name,
+        email: email,
+        googleId: googleId,
+      })
+      .then((response) => {
+        console.log("success!!!");
+      });
   };
 
   return (
@@ -14,6 +29,7 @@ const SignInPage: React.FC = () => {
         <div className="top">
           <h2>Sign In with Google</h2>
         </div>
+
         <div className="body">
           <GoogleLogin
             clientId="597764444042-sbpkrsr5mh8glt7t3qma0hl6ps4l64b5.apps.googleusercontent.com"
--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hi there");
+const db = require("./models");
+
+const { User } = require("./models");
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/signin", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const googleId = req.body.googleId;
+
+  User.create({
+    name: name,
+    email: email,
+    googleId: googleId,
+  }).catch((err) => console.log(err));
+
+  res.send("Inserted indo db");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on Port ${PORT}....`);
+db.sequelize.sync().then((req) => {
+  app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}....`);
+  });
 });
