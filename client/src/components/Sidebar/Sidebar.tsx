c1564b6c5fde5bc13e2281c81e80f091e8e6e180
‘Juliano <julianoperins@gmail.com>
2021-02-05 21:01:18 -0500

    workink on the side menu

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import "./Sidebar.css";
 
 import { SidebarData } from "./SidebarData";
 
 const Sidebar = () => {
   return (
     <div style={{ width: "200px" }}>
-      <ul>
-        {SidebarData.map((item, index) => {
-          return <li>{item.title}</li>;
+      <ul className="sidebarRows">
+        {SidebarData.map((item, key) => {
+          return (
+            <li key={key} className="row">
+              <div>{item.icon}</div>
+              <div>
+                <h2>{item.title}</h2>
+              </div>
+            </li>
+          );
         })}
       </ul>
     </div>
