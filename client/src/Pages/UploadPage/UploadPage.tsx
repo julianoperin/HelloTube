8add8dad8478f584c7e381bddd5b6fffb3134a52
‘Juliano <julianoperins@gmail.com>
2021-02-07 13:32:57 -0500

    Created the upload page, only the icon to upload left

--- client/src/Pages/UploadPage/UploadPage.tsx
+++ client/src/Pages/UploadPage/UploadPage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./UploadPage.css";
 
+import { AiOutlineClose } from "react-icons/ai";
+// import { FaBeer } from 'react-icons/fa';
+
 const UploadPage: React.FC = () => {
   return (
     <div className="UploadPage">
       <div className="SelectFileContainer">
         <div className="top-upload">
-          <div className="leftSide"></div>
-          <div className="rightSide"></div>
+          <div className="leftSide">
+            <h2>Upload Videos</h2>
+          </div>
+          <div className="rightSide">
+            <AiOutlineClose className="btn-upload" />
+          </div>
         </div>
         <div className="body-upload"></div>
       </div>
